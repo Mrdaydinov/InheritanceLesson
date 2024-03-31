e0ce6e55e904d2d982f750296ec3111278ee690b
Murad <mrdaydinov@gmail.com>
2024-03-31 16:41:18 +0400

    init commit

---
+++ InheritanceLesson/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace InheritanceLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student();
+
+            student.Grades = new int[] { 1, 2, 3, 4, 5, 7, 8, 10 };
+
+            foreach (int i in student.Grades)
+                Console.WriteLine(i);
+
+        }
+    }
+}
---
+++ InheritanceLesson/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceLesson
+{
+    internal class Student
+    {
+
+
+        private int[] grades;
+
+        public int[] Grades
+        {
+            get { return grades; }
+            set
+            {
+                int count = 0;
+                foreach (var grade in value)
+                {
+                    if (grade % 2  == 0)
+                        count++;
+                }
+                grades = new int[count];
+                int index = 0;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        grades[index] = value[i];
+                        index++;
+                    }
+                }
+            }
+        }
+
+    }
+}
